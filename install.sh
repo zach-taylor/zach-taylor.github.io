413ab285f27cb318d96d97837eaa77723796905a
Zach Taylor <ztaylor234@gmail.com>
2017-10-26 19:57:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone https://stash.zachs.info/scm/per/dotfiles.git "$HOME/.yadr"
+    git clone https://bitbucket.org/zach-taylor/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
